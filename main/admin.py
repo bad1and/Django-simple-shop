cf096a30a088717d1b36d36a56c0b3d6ded20ec9
bad1and <163727824+bad1and@users.noreply.github.com>
2025-11-19 09:59:47 +0300

    created and start adm panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug'] #параметры из моделс которые отображаются
+    prepopulated_fields = {'slug': ('name',)} # поля автоматически будут заполнены
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available', 'created', 'updated'] #декторатор
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'available'] #изменяемы параметры
+    prepopulated_fields = {'slug': ('name',)}
